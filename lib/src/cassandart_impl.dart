b56490d7f4269120baf485aba629abe064e7ac88
Alexy Marcell <alexy.marcell@hotmail.com>
2019-12-04 12:35:23 +0100

    Added _Peer class for tracking latency.
    removed notes about timeout

--- lib/src/cassandart_impl.dart
+++ lib/src/cassandart_impl.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
--- lib/src/cluster.dart
+++ lib/src/cluster.dart
@@ -101,3 +101,72 @@ class _Connection {
     await _protocol.close();
   }
 }
+
+class _Peer {
+  /// Adddress of the connection
+  final InternetAddress host;
+
+  /// Port of the connection
+  final int port;
+  final FrameProtocol _protocol;
+
+  static const int _latencyMaxLength = 100;
+  Queue<double> _lastLatencies;
+  // Average latency of the last [_latencyMaxLength] request.
+  double get latency {
+    return _lastLatencies.reduce((a, b) => a + b) / _lastLatencies.length;
+  }
+
+  _Peer._(this.host, this.port, this._protocol);
+  static Future<_Peer> connectAdress(String hostPort,
+      {@required Authenticator authenticator}) async {
+    final host = InternetAddress(hostPort.split(':').first);
+    final port = int.parse(hostPort.split(':').last);
+    return await connect(host, port, authenticator: authenticator);
+  }
+
+  static Future<_Peer> connect(InternetAddress host, int port,
+      {@required Authenticator authenticator}) async {
+    final socket = await Socket.connect(host, port);
+    final frameHandler = FrameProtocol(FrameSink(socket), parseFrames(socket));
+    await frameHandler.start(authenticator);
+    final peer = _Peer._(host, port, frameHandler);
+    peer._lastLatencies = Queue<double>();
+  }
+
+  Future _sendExecute(String query, Consistency consistency, values) async {
+    final sw = Stopwatch();
+    sw.start();
+    final result = await _protocol.execute(query, consistency, values);
+    sw.stop();
+    if(_lastLatencies.length < _latencyMaxLength) {
+      _lastLatencies.removeLast();
+    }
+    _lastLatencies.addFirst(sw.elapsedMicroseconds*1e-6);
+    return result;
+  }
+
+  Future _sendQuery(Client client, _Query q, Uint8List body) async {
+    final sw = Stopwatch();
+    sw.start();
+    final result = await _trackLatency(_protocol.query(client, q, body));
+    sw.stop();
+    if(_lastLatencies.length < _latencyMaxLength) {
+      _lastLatencies.removeLast();
+    }
+    _lastLatencies.addFirst(sw.elapsedMicroseconds*1e-6);
+    return result;
+  }
+
+  Future<R> _trackLatency<R>(Future<R> f) async {
+    final sw = Stopwatch();
+    sw.start();
+    final result = await f;
+    sw.stop();
+    if(_lastLatencies.length < _latencyMaxLength) {
+      _lastLatencies.removeLast();
+    }
+    _lastLatencies.addFirst(sw.elapsedMicroseconds*1e-6);
+    return result;
+  }
+}
