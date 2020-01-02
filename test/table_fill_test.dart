e974520e87c2d4e17068cde398fdc90722be765d
Alexy Marcell <alexy.marcell@hotmail.com>
2020-01-02 18:26:21 +0100

    Added test for filling a table with random ids.

---
+++ test/table_fill_test.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+
+import 'package:cassandart/cassandart.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  group('Table fill test', () {
+    Cluster cluster;
+
+    setUp(() async{
+      cluster = await Cluster.connect(['remote-cassandra-server:9042'],
+          authenticator: PasswordAuthenticator('cassandra', 'cassandra'));
+    });
+
+    test('Drop test', () async {
+      await cluster.execute('''
+        DROP TABLE cassandart_fill.table_fill ;
+        ''');
+      await cluster.execute('''
+        DROP KEYSPACE cassandart_fill ;
+        ''');
+    });
+
+    test('Create test', () async {
+      await cluster.execute('''
+        CREATE KEYSPACE IF NOT EXISTS cassandart_fill 
+        WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
+        ;
+        ''');
+      await cluster.execute('''
+        CREATE TABLE cassandart_fill.table_fill (
+          id text PRIMARY KEY)
+        ;
+        ''');
+    });
+
+    test('Insert test', () async {
+      final random = Random();
+      for(int i = 0; i < 50; i++) {
+        final len = random.nextInt(12) + 3;
+        final chars = <int>[];
+        for(int j = 0; j < len; j++) {
+          chars.add(33 + random.nextInt(94));
+        }
+        final id = String.fromCharCodes(chars);
+        await cluster.execute('''
+        INSERT INTO cassandart_fill.table_fill
+        (id)
+        VALUES (:id)
+        ;
+        ''',
+            values: {'id': id});
+        print('$i: $id');
+      }
+    });
+
+  });
+}
